02158d2746c4aec812068da1cbc9ec6d6ff3fbb7
Loic Verrall <loic.verrall@gmail.com>
2018-10-14 12:36:44 +0100

    Initial Commit

---
+++ UniNinja/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UniNinja
+//
+//  Created by Loic Verrall on 14/10/2018.
+//  Copyright © 2018 UniNinja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
